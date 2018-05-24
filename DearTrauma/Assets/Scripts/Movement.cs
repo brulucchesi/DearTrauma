6aff0e544af6bead7eccb71076c27cab4cc75388
Felipe Pessoa <felipessoaf@hotmail.com>
2018-05-23 23:47:01 -0300

    change movement

--- DearTrauma/Assets/Scripts/Movement.cs
+++ DearTrauma/Assets/Scripts/Movement.cs
@@ -21,17 +21,23 @@ public class Movement : MonoBehaviour {
     void Update()
     {
         float hor = Input.GetAxis("Horizontal");
-        if(hor>0)
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             Right = true;
+            rb.velocity = new Vector2(Speed, rb.velocity.y);
         }
-        if (hor < 0)
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             Right = false;
+            rb.velocity = new Vector2(-Speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
-        Vector2 vel = new Vector2((hor == 1 || hor == -1)? hor * Speed * 1.5f: hor * Speed, rb.velocity.y);
+        //Vector2 vel = new Vector2((hor == 1 || hor == -1)? hor * Speed * 1.5f: hor * Speed, rb.velocity.y);
 
-        rb.velocity = vel;
+        //rb.velocity = vel;
 	}
 }
