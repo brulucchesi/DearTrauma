ca8b6cd3f83cccbdb9d16a84b07b344bd1ce7623
Felipe Pessoa <felipessoaf@gmail.com>
2018-06-04 18:32:35 -0300

    cam follow

---
+++ DearTrauma/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour {
+    
+    [Header("References")]
+    public Transform Target;
+
+    [Header("Modifiers")]
+    public float smoothTime = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        Vector3 targetPosition = Target.TransformPoint(new Vector3(0, 2, -10));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
