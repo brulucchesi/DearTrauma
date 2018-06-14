d17781e7d9a31266afcbc08eaa58100cef1f0b0a
Felipe Pessoa <felipessoaf@gmail.com>
2018-06-14 15:55:54 -0300

    breakable

---
+++ DearTrauma/Assets/Scripts/Breakable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+
+    public void ReceiveAttack()
+    {
+        GetComponent<Animator>().SetTrigger("Break");
+    }
+}
