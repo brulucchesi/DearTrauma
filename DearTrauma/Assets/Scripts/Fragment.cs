1f7e6cc2ebafa55becf903d0ada1f286ff2104f4
Felipe Pessoa <felipessoaf@gmail.com>
2018-06-14 16:04:52 -0300

    fragment visual set active

--- DearTrauma/Assets/Scripts/Fragment.cs
+++ DearTrauma/Assets/Scripts/Fragment.cs
@@ -6,12 +6,24 @@ public class Fragment : MonoBehaviour {
 
     [Header("References")]
     public Door LinkedDoor;
+    public GameObject FragmentMemoryVisual;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            LinkedDoor.Unlock();
+            if(LinkedDoor)
+            {
+                LinkedDoor.Unlock();
+            }
+            if (FragmentMemoryVisual)
+            {
+                FragmentMemoryVisual.SetActive(true);
+            }
+            if (LinkedDoor)
+            {
+                LinkedDoor.Unlock();
+            }
             gameObject.SetActive(false);
         }
     }
