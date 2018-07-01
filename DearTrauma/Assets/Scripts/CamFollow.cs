0f8d35c70be8aa22c31099a7a1baaf1270047a67
Felipe Pessoa <felipessoaf@hotmail.com>
2018-07-01 15:54:36 -0300

    evolution

--- DearTrauma/Assets/Scripts/CamFollow.cs
+++ DearTrauma/Assets/Scripts/CamFollow.cs
@@ -8,15 +8,33 @@ public class CamFollow : MonoBehaviour {
     public Transform Target;
 
     [Header("Modifiers")]
-    public float smoothTime = 0.3f;
-    public float camY = 2f;
-    public float camX = 0f;
+    public float SmoothTime = 0.3f;
+    public float CamY = 1.64f;
+    public float CamX = 2.05f;
+    public float CamYBig = 0.4f;
+    public float CamXBig = 2.05f;
+    public float SizeBig = 7.5f;
 
     private Vector3 velocity = Vector3.zero;
+    private float camY;
+    private float camX;
+
+    private void Start()
+    {
+        camY = CamY;
+        camX = CamX;
+    }
 
     void Update()
     {
-        Vector3 targetPosition = Target.TransformPoint(new Vector3(camX, camY, -10));
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        Vector3 targetPosition = Target.TransformPoint(new Vector3(CamX, CamY, -10));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
+    }
+
+    public void Big()
+    {
+        camY = CamYBig;
+        camX = CamXBig;
+        Camera.main.orthographicSize = SizeBig;
     }
 }
---
+++ DearTrauma/Assets/Scripts/Player/Evolution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evolution : MonoBehaviour {
+
+    [Header("References")]
+    public RuntimeAnimatorController BigAnimator;
+
+    [Header("Modifiers")]
+    public float ScaleMultiplier = 2f;
+
+    private int fragmentCounter;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Fragment"))
+        {
+            fragmentCounter++;
+            if(fragmentCounter == 3)
+            {
+                GetComponent<Animator>().runtimeAnimatorController = BigAnimator;
+                transform.localScale *= ScaleMultiplier;
+                Camera.main.GetComponent<CamFollow>().Big();
+                //transform.Translate(transform.position + (Vector3.up * 2));
+            }
+        }
+    }
+}
