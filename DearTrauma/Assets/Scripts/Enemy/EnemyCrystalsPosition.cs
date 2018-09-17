57fe294880873499eba8ab07c7fef8ffdabce52e
DESKTOP-BFU1N5B\gabi1 <gabrielle189189@gmail.com>
2018-09-17 17:36:33 -0300

    enemy crystals

---
+++ DearTrauma/Assets/Scripts/Enemy/EnemyCrystalsPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vigiaCrystalsPosition : MonoBehaviour {
+
+    public GameObject track;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = track.transform.position;
+
+	}
+}
