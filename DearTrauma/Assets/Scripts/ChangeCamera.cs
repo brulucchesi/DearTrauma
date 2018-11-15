3215e62a1138987a16dc580461fa1c974e321bcf
Bruna Lucchesi <brunalvillela@gmail.com>
2018-11-15 19:31:47 -0200

    fix camera and fix jump

--- DearTrauma/Assets/Scripts/ChangeCamera.cs
+++ DearTrauma/Assets/Scripts/ChangeCamera.cs
@@ -14,6 +14,8 @@ public class ChangeCamera : MonoBehaviour
 
     private bool timelineHasPlayed = false;
 
+    private bool timelineIsReseted = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
@@ -26,13 +28,17 @@ public class ChangeCamera : MonoBehaviour
             {
                 if (!timelineHasPlayed)
                 {
+                    Debug.Log("alo to tocanu");
                     timelineHasPlayed = true;
                     Debug.Log("play animation from camera");
                     ThisTimeline.Play();
-                } else
+                }
+                else
                 {
+                    ThisCamera.GetComponent<Cinemachine.CinemachineVirtualCamera>().enabled = false;
+                    Main.GetComponent<Cinemachine.CinemachineVirtualCamera>().enabled = true;
+                    Main.GetComponent<Cinemachine.CinemachineVirtualCamera>().MoveToTopOfPrioritySubqueue();
                     Debug.Log("animation da camera já deu play");
-
                 }
 
             }
@@ -46,6 +52,19 @@ public class ChangeCamera : MonoBehaviour
             Debug.Log("Leave");
             ThisCamera.GetComponent<Cinemachine.CinemachineVirtualCamera>().enabled = false;
             Main.GetComponent<Cinemachine.CinemachineVirtualCamera>().MoveToTopOfPrioritySubqueue();
+        }
+    }
+
+    private void Update()
+    {
+        if(ThisTimeline != null)
+        {
+            if (ThisCamera.GetComponent<PlayableDirector>().state != PlayState.Playing && timelineHasPlayed && !timelineIsReseted)
+            {
+                timelineIsReseted = true;
+                ThisCamera.GetComponent<Cinemachine.CinemachineVirtualCamera>().enabled = false;
+                Main.GetComponent<Cinemachine.CinemachineVirtualCamera>().MoveToTopOfPrioritySubqueue();
+            }
         }
     }
 }
