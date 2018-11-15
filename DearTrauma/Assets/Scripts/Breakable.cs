43fac49c8c15196ff0d67fec5af676ee0439abc8
Felipe Pessoa <felipessoaf@hotmail.com>
2018-11-15 11:04:08 -0200

    breakable sound

--- DearTrauma/Assets/Scripts/Breakable.cs
+++ DearTrauma/Assets/Scripts/Breakable.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Breakable : MonoBehaviour {
+public class Breakable : MonoBehaviour
+{
+    [Header("References")]
+    public AudioSource Audio;
 
     public void ReceiveAttack()
     {
         GetComponent<Animator>().SetTrigger("Break");
+        Audio.Play();
     }
 }
