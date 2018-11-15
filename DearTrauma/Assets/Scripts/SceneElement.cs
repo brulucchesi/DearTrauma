83ae034bc3a0c4ea46b388da21b9197dc319489e
Felipe Pessoa <felipessoaf@hotmail.com>
2018-11-15 11:08:21 -0200

    scene element prefab (sound)

---
+++ DearTrauma/Assets/Scripts/SceneElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneElement : MonoBehaviour
+{
+    [Header("References")]
+    public AudioSource Audio;
+
+    public void PlaySound()
+    {
+        Audio.Play();
+    }
+}
