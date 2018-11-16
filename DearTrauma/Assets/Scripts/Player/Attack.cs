caad331b6399ac60aef4f68bc7b6535b7148fe2e
Felipe Pessoa <felipessoaf@gmail.com>
2018-11-16 16:37:56 -0200

    particle attack change

--- DearTrauma/Assets/Scripts/Player/Attack.cs
+++ DearTrauma/Assets/Scripts/Player/Attack.cs
@@ -7,8 +7,8 @@ public class Attack : MonoBehaviour {
     [Header("References")]
     public GameObject AttackPoint;
     public AudioSource AttackAudio;
-    public GameObject AttackLeft;
-    public GameObject AttackRight;
+    public ParticleSystem AttackLeft;
+    public ParticleSystem AttackRight;
 
     [Header("Modifiers")]
     public LayerMask Mask;
@@ -75,19 +75,21 @@ public class Attack : MonoBehaviour {
     {
         if(GetComponent<Movement>().Right)
         {
-            AttackRight.SetActive(true);
-            AttackLeft.SetActive(false);
+            AttackRight.Stop();
+            AttackRight.Play();
+            AttackLeft.Stop();
         }
         else
         {
-            AttackLeft.SetActive(true);
-            AttackRight.SetActive(false);
+            AttackLeft.Stop();
+            AttackLeft.Play();
+            AttackRight.Stop();
         }
     }
 
     public void AttackParticleEnd()
     {
-        AttackLeft.SetActive(false);
-        AttackRight.SetActive(false);
+        AttackRight.Stop();
+        AttackLeft.Stop();
     }
 }
