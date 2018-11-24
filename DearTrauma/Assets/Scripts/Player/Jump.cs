9aeeddc417a886ea7b50a89b68c2aed4f20b6fbc
Felipe Pessoa <felipessoaf@hotmail.com>
2018-11-24 20:09:22 -0200

    land sound

--- DearTrauma/Assets/Scripts/Player/Jump.cs
+++ DearTrauma/Assets/Scripts/Player/Jump.cs
@@ -110,6 +110,11 @@ public class Jump : MonoBehaviour
                 canResetJump = false;
 
                 GetComponent<Movement>().StopWalkAudio(false);
+                Land();
+            }
+            if (!anim.GetBool("grounded"))
+            {
+                Land();
             }
             anim.SetBool("grounded", true);
         }
