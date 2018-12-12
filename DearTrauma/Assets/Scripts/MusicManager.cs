2992ff56682c86039f3966d8dfac0405e8899856
Bruna Lucchesi <brunalvillela@gmail.com>
2018-12-12 00:49:16 -0200

    fixes

--- DearTrauma/Assets/Scripts/MusicManager.cs
+++ DearTrauma/Assets/Scripts/MusicManager.cs
@@ -5,28 +5,25 @@ using UnityEngine;
 public class MusicManager : MonoBehaviour
 {
     public AudioSource BackgroundMusic;
-    public AudioSource BackgroundMusicAux;
     public AudioClip Title;
     public AudioClip GamePlay;
     public AudioClip Credits;
     public AudioClip EmptySound;
 
-	void Awake ()
-	{
-	    BackgroundMusic.clip = Title;
+    void Awake()
+    {
+        BackgroundMusic.clip = Title;
         BackgroundMusic.Play();
     }
 
     public void ChangeGamePlay()
     {
-        BackgroundMusicAux.clip = GamePlay;
-        BackgroundMusicAux.Play();
+        BackgroundMusic.clip = GamePlay;
+        BackgroundMusic.Play();
     }
 
     public void ChangeBoss()
     {
-        BackgroundMusicAux.clip = EmptySound;
-        BackgroundMusicAux.Stop();
         BackgroundMusic.clip = EmptySound;
         BackgroundMusic.gameObject.SetActive(false);
         BackgroundMusic.gameObject.SetActive(true);
